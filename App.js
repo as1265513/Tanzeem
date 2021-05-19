e8015a41471dc962970a158062a06267c5671438
Azeem Sarwar <as1265513@gmail.com>
2021-05-19 14:22:19 +0500

    Intial Setup
    Done

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from './src/SplashScreen/SplashScreen'
+import Login from './src/Login/Login'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [active,setActive] = useState(false)
+    useEffect(() => {
+
+        setTimeout(() => {
+            setActive(true)
+        }, 5000);
+    }, [])
+    if(!active)
+    return <SplashScreen />
+    else 
+    return <Login />
 }
 
 const styles = StyleSheet.create({
