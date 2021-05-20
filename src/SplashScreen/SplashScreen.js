b7b7c6b6d773c1153be53f0abe5e07d462416e77
Azeem Sarwar <as1265513@gmail.com>
2021-05-20 11:33:54 +0500

    Initial setup
    ok

--- src/SplashScreen/SplashScreen.js
+++ src/SplashScreen/SplashScreen.js
@@ -1,11 +1,32 @@
 import React,{useEffect,useState} from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet,SafeAreaView,Platform } from 'react-native'
 
 export default function SplashScreen() {
     
     return (
-        <View>
-            <Text>Starting Screen</Text>
+        <View style={styles.Container}>
+            <View style={styles.UpperContainer} />
+
+            <View style={styles.middleContainer}>
+
+            </View>
+
+            <View style={styles.LowerContainer} />
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    Container:{
+        flex:1,
+        ...Platform.select({
+            android:{
+                marginTop:30
+            }
+        })
+    },
+    UpperContainer:{
+        
+    }
+})
