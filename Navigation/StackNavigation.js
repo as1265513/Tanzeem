65d2e7b59bb11ec306445a2e26906a103a08d6a5
Rehan Asghar <72587992+Rehan-05@users.noreply.github.com>
2021-05-29 15:24:34 +0500

    Update StackNavigation.js

--- Navigation/StackNavigation.js
+++ Navigation/StackNavigation.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+<<<<<<< Updated upstream
 import SplashScreen from "../src/SplashScreen/SplashScreen";
+=======
+>>>>>>> Stashed changes
 import Login from "../src/Login/Login";
 import Signup from "../src/Signup/Signup";
 
@@ -9,6 +12,7 @@ const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
   return (
+<<<<<<< Updated upstream
     <Stack.Navigator>
       <Stack.Screen name="SplashScreen" component={SplashScreen} 
           options={{
@@ -19,6 +23,13 @@ const MainStackNavigator = () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
+=======
+     <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
+
+>>>>>>> Stashed changes
   );
 }
 
